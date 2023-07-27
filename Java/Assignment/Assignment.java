16e8a39d4b1dc4611db0fab6d0cd313eb0b7f1ef
hengA2333 <13536036212@163.com>
2023-07-27 16:29:51 +0800

    7.27_change

--- Java/Assignment/Assignment.java
+++ Java/Assignment/Assignment.java
@@ -13,6 +13,7 @@ public class Assignment {
         t2.level = 47;
         System.out.println("1: t1.level: " + t1.level + ", t2.level: " + t2.level);
         //此处即为引用，java自动清除了t1的原值，此时t1和t2的对象相同
+        //若仅想赋值，可写t1.level=t2.level
         t1 = t2;
         System.out.println("2: t1.level: " + t1.level + ", t2.level: " + t2.level);
         //故更改t1实质上是更改t2，因此t1与t2的level值相同
