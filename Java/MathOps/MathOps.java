190adef5b472c41d8428ace511f6639a076f6193
hengA2333 <13536036212@163.com>
2023-07-27 17:15:21 +0800

    7.27

---
+++ Java/MathOps/MathOps.java
@@ -0,0 +1,49 @@
+package Java.MathOps;
+
+import java.util.*;
+
+public class MathOps {
+    public static void main(String[] args) {
+        //创建rand量，种子可提供可不提供，不提供则以时间为种子
+        Random rand = new Random(47);
+        int i, j, k;
+        //可利用nextInt()或nextFloat()等生成随机数，其中的bound即为上限
+        j = rand.nextInt(100) + 1;
+        System.out.println("j : " + j);
+        k = rand.nextInt(100) + 1;
+        System.out.println("k = " + k);
+        i = j + k;
+        System.out.println("j + k : " + i);
+        i = j - k;
+        System.out.println("j - k : " + i);
+        i = k / j;
+        System.out.println("k / j : " + i);
+        i = k * j;
+        System.out.println("k * j : " + i);
+        i = k % j;
+        System.out.println("k % j : " + i);
+        j %= k;
+        System.out.println("j %= k : " + j);
+        float u, v, w;
+        v = rand.nextFloat();
+        System.out.println("v : " + v);
+        w = rand.nextFloat();
+        System.out.println("w : " + w);
+        u = v + w;
+        System.out.println("v + w : " + u);
+        u = v - w;
+        System.out.println("v - w : " + u);
+        u = v * w;
+        System.out.println("v * w : " + u);
+        u = v / w;
+        System.out.println("v / w : " + u);
+        u += v;
+        System.out.println("u +=v : " + u);
+        u -= v;
+        System.out.println("u -= v : " + u);
+        u *= v;
+        System.out.println("u *= v : " + u);
+        u /= v;
+        System.out.println("u /= v : " + u);
+    }
+}
