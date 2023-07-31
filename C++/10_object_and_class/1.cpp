60802e345e140530174e151d92bee3f1d1435ebe
hengA2333 <13536036212@163.com>
2023-07-31 15:08:57 +0800

    7.31

---
+++ C++/10_object_and_class/1.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+#include <string>
+//定义类Stock
+class Stock
+{
+private:
+    std::string company;
+    long shares;
+    double share_val;
+    double total_val;
+    void set_tot() { total_val = shares * share_val; }
+//set_tot函数也可在类外定义，可写inline set_tot()
+//public部分可访问
+public:
+    void acquire(const std::string &co, long n, double pr);
+    void buy(long num, double price);
+    void sell(long num, double price);
+    void update(double price);
+    void show();
+};
+//定义各函数
+void Stock::acquire(const std::string &co, long n, double pr)
+{
+    company = co;
+    if (n<0)
+    {
+        std::cout << "Number of shares can't be negative; "
+                  << company << " shares set to 0\n";
+        shares = 0;
+    }
+    else
+    {
+        shares = n;
+    }
+    share_val = pr;
+    set_tot();
+}
+
+void Stock::buy(long num, double price)
+{
+    if (num<0)
+    {
+        std::cout << "Number of shares purchased can't be negative. "
+                  << "Transaction is aborted.\n";
+    }
+    else
+    {
+        shares += num;
+        share_val = price;
+        set_tot();
+    }
+}
+
+void Stock::sell(long num, double price)
+{
+    using std::cout;
+    if (num<0)
+    {
+        cout << "Number of shares sold can't be negative. "
+             << "Transaction is aborted.\n";
+    }
+    else if (num>shares)
+    {
+        cout << "You can't sell more than you have! "
+             << "Transaction is aborted.\n";
+    }
+    else
+    {
+        shares -= num;
+        share_val = price;
+        set_tot();
+    }
+}
+
+void Stock::update(double price)
+{
+    share_val = price;
+    set_tot();
+}
+
+void Stock::show()
+{
+    //设定输出格式
+    using std::cout;
+    using std::ios_base;
+    ios_base::fmtflags orig =
+        cout.setf(ios_base::fixed, ios_base::floatfield);
+    std::streamsize prec = cout.precision(3);
+
+    std::cout << "Company: " << company << " Shares: " << shares << '\n'
+              << " Shares Price: " << share_val << " Total Worth: " << total_val << '\n';
+    //恢复原格式
+    cout.setf(orig, ios_base::floatfield);
+    cout.precision(prec);
+}
+
+int main()
+{
+    Stock fluffy_the_cat;
+    fluffy_the_cat.acquire("NanoSmart", 20, 12.50);
+    fluffy_the_cat.show();
+    fluffy_the_cat.buy(15,18.125);
+    fluffy_the_cat.show();
+    fluffy_the_cat.sell(400, 20.00);
+    fluffy_the_cat.show();
+    fluffy_the_cat.buy(300000, 40.125);
+    fluffy_the_cat.show();
+    fluffy_the_cat.sell(300000, 0.125);
+    fluffy_the_cat.show();
+    return 0;
+}
