4a4daded0bb7dd3a4c0b4a056891ce6273488cc0
hengA2333 <13536036212@163.com>
2023-08-22 09:43:39 +0800

    8.22update

--- C++/04_array/20.cpp
+++ C++/04_array/20.cpp
@@ -13,6 +13,8 @@ int main()
     cout << "p3[1] is " << p3[1] << ".\n";
     // 指针减一即变为前一个元素的地址
     p3 = p3 - 1;
+    cout << "Now p3[0] is " << p3[0] << " and ";
+    cout << "p3[1] is " << p3[1] << ".\n";
     delete[] p3;
     return 0;
 }
